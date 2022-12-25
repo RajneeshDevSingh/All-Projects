824d95057290b5e9cc386d0be1181bf0da7a773d
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-12-26 00:31:45 +0530

    16

--- Front-End-Project/expense_tracker/src/components/Expenses/Expenses.js
+++ Front-End-Project/expense_tracker/src/components/Expenses/Expenses.js
@@ -5,6 +5,8 @@ import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
+import ExpensesChart from './ExpensesChart';
+
 const Expenses=(props)=> {
   const [filteredYear, setFilteredYear] = useState('2020');
 
@@ -20,6 +22,7 @@ const Expenses=(props)=> {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses ={filteredExpenses}/>
       <ExpensesList items={filteredExpenses}/>
     </Card>
   );
