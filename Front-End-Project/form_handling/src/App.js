d72389307c86b561dd79573550d76ddc119f20c5
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-01-07 11:28:56 +0530

    12

--- Front-End-Project/form_handling/src/App.js
+++ Front-End-Project/form_handling/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import AddUser from './Component/Users/AddUser';
+import UsersList from './Component/Users/UsersList';
 function App() {
   return (
     <div className="">
         <AddUser/>
+        <UsersList users = {[]}/>
+
     </div>
   );
 }
