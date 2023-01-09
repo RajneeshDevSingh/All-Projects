026f188aea44f736e7736a23e6bde0467b9b1ccd
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-01-10 02:00:12 +0530

    26

--- Others/form/src/component/Navbar/Navbar.js
+++ Others/form/src/component/Navbar/Navbar.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React , {useState}from 'react'
 import "./Navbar.css"
 function Navbar() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedin, setIsLoggedin] = useState(false);
+ 
+  const login = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+    const userData = {
+      name,
+      email,
+      password,
+    };
+    localStorage.setItem('token-info', JSON.stringify(userData));
+    setIsLoggedin(true);
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+ 
+  const logout = () => {
+    localStorage.removeItem('token-info');
+    setIsLoggedin(false);
+  };
   return (
     <div className=''>
       <nav>
@@ -23,13 +48,43 @@ function Navbar() {
             <i className="fa-solid fa-times"></i>
         </div> */}
       </nav>
-      <div className='form_container'>
-        <form>
-          <input type="text"></input>
-          <input type="password"></input>
-          <button type='submit'>Sign in</button>
-        </form>
-      </div>
+      <div style={{ textAlign: 'center' }}>
+      <h1>This is React WebApp </h1>
+      {!isLoggedin ? (
+        <>
+          <form action="">
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Password"
+            />
+            <button type="submit" onClick={login}>
+              GO
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1>User is logged in</h1>
+          <button onClickCapture={logout}>logout user</button>
+        </>
+      )}
+    </div>
+      
+      
     </div>
   )
 }
