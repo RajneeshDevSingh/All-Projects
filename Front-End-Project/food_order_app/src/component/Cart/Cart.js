5bbe9a3b5908f3d10ba04c6422c2f3e12627f8f6
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-01-14 14:43:47 +0530

    8

---
+++ Front-End-Project/food_order_app/src/component/Cart/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import classes from "./Cart.module.css"
+const Cart = (props) => {
+  return (
+    <div>
+        CartItems
+        <div></div>
+        <div></div>
+        <div></div>
+        
+    </div>
+  )
+}
+
+export default Cart
