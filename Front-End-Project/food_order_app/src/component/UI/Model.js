8ae5b8f14e144dc8ac05323bc78583ac5a0a7a7d
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-01-14 14:59:45 +0530

    9

---
+++ Front-End-Project/food_order_app/src/component/UI/Model.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
+
+
+import React from 'react'
+
+const Model = () => {
+  return (
+    <div>Model</div>
+  )
+}
+
+export default Model
