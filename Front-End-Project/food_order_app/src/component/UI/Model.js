2a97bb6bcf411b15659e561b184479d23e279dd9
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-01-14 15:18:48 +0530

    10

--- Front-End-Project/food_order_app/src/component/UI/Model.js
+++ Front-End-Project/food_order_app/src/component/UI/Model.js
@@ -2,13 +2,34 @@ import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
-
-
 import React from 'react'
 
+
+const Backdrop = (props)=>
+{
+    return(
+        <div className={classes.backdrop}>
+
+        </div>
+    )
+}
+const ModalOverlay= () =>
+{
+    return(
+        <div className={classes.model}>
+            <div className={classes.content}>
+                {props.children}
+            </div>
+
+        </div>
+    )
+}
 const Model = () => {
   return (
-    <div>Model</div>
+    <Fragment>
+        <Backdrop/>
+        <ModalOverlay/>
+    </Fragment>
   )
 }
 
