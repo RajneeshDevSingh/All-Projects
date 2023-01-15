f4206d4cab328d0d92934816aea759adb337ae51
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-01-15 20:32:35 +0530

    12

--- Front-End-Project/food_order_app/src/App.js
+++ Front-End-Project/food_order_app/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './component/Layout/Header';
 import Meals from './component/Meals/Meals';
+import Cart from './component/UI/Card';
 function App() {
   return (
     <React.Fragment>
+      <Cart/>
       <Header/>
       <main>
           <Meals/>
--- Front-End-Project/food_order_app/src/component/Cart/Cart.js
+++ Front-End-Project/food_order_app/src/component/Cart/Cart.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import Model from '../UI/Model'
-import classes from "./Cart.module.css"
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
 const Cart = (props) => {
-    const cartItems = (
-        <ul className={classes['cart-items']}>
-          {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-            <li>{item.name}</li>
-          ))}
-        </ul>
-    )
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
   return (
-    <Model>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>35.9</span>
-        </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes.button}>Order</button>
-        </div>
-        <div></div>
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
 
-    </Model>
-  )
-}
-
-export default Cart
+export default Cart;
