70d0db83cbf2a0f8ccaff2b7cc88ab38361f6267
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2023-11-01 14:37:55 +0530

    1

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -7,7 +7,7 @@ function RestaurantCategory({data, showItems , setShowIndex}) {
   {
     setShowIndex();
   }
-
+ // just check
   return (
     
       <div className="p-4 mx-auto my-4 max-w-6xl rounded-xl  shadow-xl bg-stone-200-300 border-solid border-4 border-white">
