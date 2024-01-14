e7c4bcd06833d4c1bea164468367ceebf7949694
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2024-01-15 02:33:24 +0530

    Store Configured

--- Front-End-Project/youtube_clone/src/App.js
+++ Front-End-Project/youtube_clone/src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import Head from "./components/Head"
 
 import Body from "./components/Body";
-
-
+import Store from "./Utils/Store";
+import { Provider } from "react-redux";
 function App() {
   return (
+    <Provider store={Store}>
     <div className="">
       <Head/>
       <Body/>
 
     </div>
+    </Provider>
   );
 }
 
