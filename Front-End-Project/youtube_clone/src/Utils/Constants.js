8d40c67f01c228221d2bdc272b90bf5ef3993545
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2024-01-28 22:25:31 +0530

    API Call OK1

--- Front-End-Project/youtube_clone/src/Utils/Constants.js
+++ Front-End-Project/youtube_clone/src/Utils/Constants.js
@@ -1,5 +1,5 @@
 
-const API_Key = "AIzaSyCfXXzU7wimn1HnC_vecAn1ss7Ss0DDyUs";
+const API_Key = "AIzaSyDWAnlIDosHMmxtfklaI6S-s68uR_vDSDM";
 const Youtube_API_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+API_Key;
 // const API_Key = "AIzaSyC7B6bh9m8qhZXKxbGuImCxAQ3gkblWaVU";
--- Front-End-Project/youtube_clone/src/components/VideoContainer.js
+++ Front-End-Project/youtube_clone/src/components/VideoContainer.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Youtube_API_URL from '../Utils/Constants'
 
 const VideoContainer = () => {
+useEffect(()=>{
+  GetVideo();
+},[])
+
+const GetVideo = async ()=>
+{
+  const data = await fetch(Youtube_API_URL);
+
+  const Json = await data.json();
+
+  console.log(Json)
+}
   return (
     <div>VideoContainer</div>
   )
