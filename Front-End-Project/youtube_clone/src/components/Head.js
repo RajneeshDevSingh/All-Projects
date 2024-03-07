e7fbffa6779fb07c31c55c068d1c6ef4786c1fcc
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2024-03-07 10:26:20 +0530

    YouTube Route deBouncing Done

--- Front-End-Project/youtube_clone/src/components/Head.js
+++ Front-End-Project/youtube_clone/src/components/Head.js
@@ -4,23 +4,27 @@ import Logo from "../img/logo1.jpg";
 
 import { useState } from "react";
 import "../Allcss/Head.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../Utils/appSlice";
 // import { Youtube_Search_Suggestion_API } from "../Utils/Constants";
 
 import { useEffect } from "react";
 import fetchJsonp from "fetch-jsonp";
+import { cacheResults } from "../Utils/searchSlice";
+
 const Head = () => {
-  const dispatch = useDispatch();
-  const toggleMenuHandler = () => {
-    dispatch(toggleMenu());
-  };
   const [searchQuery, SetSearchQuery] = useState("");
   const [suggestions , SetSuggestions] = useState([]);
   const[showsuggestions, SetShowsuggestions] = useState(false)
+  const searchCache = useSelector((store)=> store.search)
+  const dispatch = useDispatch();
+
+  
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
 
 
- 
   // make an api call after every key press, but if the difference beween 2 api calls is <200ms decline the api call , if not make the api call
 
   // *key - i
@@ -35,14 +39,26 @@ const Head = () => {
   // start timer make api call after 200 ms
 
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestion(), 200);
+    const timer = setTimeout(() =>{
+      
+      if(searchCache[searchQuery])
+      {
+        SetSuggestions(searchCache[searchQuery])
+      }
+      else
+      {
+        getSearchSuggestion()
+      }
+    
+  }, 200);
 
     return () => {
       clearTimeout(timer);
     };
   }, [searchQuery]);
 
-  let getSearchSuggestion = async () => {
+  const getSearchSuggestion = async () => {
+    console.log("API call", searchQuery)
     try {
       const response = await fetchJsonp(
         `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${searchQuery}`
@@ -50,6 +66,8 @@ const Head = () => {
       const data = await response.json();
       SetSuggestions(data[1])
       // console.log(data[1]);
+      // updated the searchCache
+      dispatch(cacheResults({[searchQuery] : data[1]}));
     } catch (error) {
       console.error("Error fetching search suggestions:", error);
     }
--- Front-End-Project/youtube_clone/src/components/WatchPage.js
+++ Front-End-Project/youtube_clone/src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../Utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentContainer from './CommentContainer';
 
 import "../Allcss/watchPage.css"
 const WatchPage = ()=>
@@ -12,6 +13,7 @@ const WatchPage = ()=>
         dispatch(closeMenu())
     },[]);
  return (
+    <div className='watchPage'>
     <div className="watchVideo">
         <iframe width="1000" 
         height="500" 
@@ -21,6 +23,10 @@ const WatchPage = ()=>
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         allowFullScreen>
         </iframe>
+    </div>
+    <div className='commentsSection'>
+        <CommentContainer/>
+    </div>
     </div>
  )
 }
