04149b2388d0199e105eb1f1d3452c58278161d5
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2024-03-11 11:02:45 +0530

    YouTube Comments

--- Front-End-Project/youtube_clone/src/components/CommentContainer.js
+++ Front-End-Project/youtube_clone/src/components/CommentContainer.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+
+import "../Allcss/Comment.css";
+const commentData = [
+  {
+    name: "Rajneesh",
+    text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+    replies: [{
+      name: "First Copy",
+      text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+      replies: [],
+    },
+    {
+      name: "Second Copy",
+      text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+      replies: [],
+    },
+    {
+      name: "Third Copy",
+      text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+      replies: [],
+    },],
+  },
+  {
+    name: "ROOO",
+    text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+    replies: [],
+  },
+  {
+    name: "GGGGG",
+    text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+    replies: [],
+  },
+  {
+    name: "nnnn",
+    text: "ygvhj hbjhbkjb uhjhjh jvh vvjh ",
+    replies: [],
+  },
+];
+
+const SingleComment = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="SingleComment">
+      <img
+        className="commentIcon"
+        alt="User"
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+      />
+      <div className="commnetdata">
+        <p className="name">{name}</p>
+        <p className="text">{text}</p>
+      </div>
+      {/* <div className="replies">
+        <p>{replies}</p>
+      </div> */}
+    </div>
+  );
+};
+
+const CommentsList = ({ Comments }) => {  
+  return Comments.map((Comment, index) => (
+    <div>
+      <SingleComment data={Comment} key={index} />
+      <div className="repliesCont">
+        <CommentsList Comments={Comment.replies} />
+ 
+      </div>
+    </div>
+  ));
+};
 
 const CommentContainer = () => {
   return (
-    <div>CommentContainer</div>
-  )
-}
+    <div className="CommentDiv">
+      <h1 className="comment">Comments</h1>
+      <CommentsList Comments={commentData} />
+    </div>
+  );
+};
 
-export default CommentContainer
+export default CommentContainer;
